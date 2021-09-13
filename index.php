66d9be2be677dae552d42be24e775dd1644cdd83
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-14 01:39:49 +0530

    pagination from datatables, edit button created,

--- index.php
+++ index.php
@@ -18,12 +18,15 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Sorry we fail to connect: " . mysqli_connect_error());
 }
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+// ////////////////////////////////////INSERT POST METHOD FUNCTION///////////////////////////
+if ($_SERVER['REQUEST_METHOD'] == 'POST'        ) {
     $ititle = $_POST['new_title'];
     $idescription = $_POST['new_description'];
-
+    
     // SQL query to be executed
+    
     $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$ititle', '$idescription')";
+    
     $result = mysqli_query($conn, $sql);
 
     // connection success and error message
@@ -33,69 +36,100 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo " unable to insert due to error: " . mysqli_error($conn);
     }
+
 }
+// ////////////////////////////////////INSERT POST METHOD FUNCTION///////////////////////////
+
 ?>
-<!-- --------------------PHP Code ---------------------------------- -->
-<!doctype html>
-<html lang="en">
+    <!-- --------------------PHP Code ---------------------------------- -->
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <!-- Jquery  -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <!-- Jquery  -->
-    
-    <!-- Links from datatable.com -->
-    <link rel="stylesheet" href="//cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
+        <!-- Jquery  -->
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <!-- Jquery  -->
 
-    <script src="//cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#myTable').DataTable();
-        });
-    </script>
+        <!-- Links from datatable.com -->
+        <link rel="stylesheet" href="//cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
 
-    <!-- Links from datatable.com -->
+        <script src="//cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#myTable').DataTable();
+            });
+        </script>
 
-    <title>iNotes - notes taking easy</title>
-</head>
+        <!-- Links from datatable.com -->
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">iNotes</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact us</a>
-                    </li>
+        <title>iNotes - notes taking easy</title>
 
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+    </head>
+
+    <body>
+        <!-- =================================MODAL=================================== -->
+        <!-- Edit modal -->
+        <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editmodal">
+  Edit modal
+</button> -->
+
+        <!-- Edit Modal -->
+        <div class="modal fade" id="editmodal" tabindex="-1" aria-labelledby="editmodalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editmodalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+        <!-- =================================MODAL=================================== -->
 
-    <!-- =====================Alert===================== -->
-    <?php
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">iNotes</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact us</a>
+                        </li>
+
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+        <!-- =====================Alert===================== -->
+        <?php
     if ($insert) {
         echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
         <strong>Success</strong> Your note has been inserted successfully.
@@ -103,62 +137,78 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       </div>";
     }
     ?>
-    <!-- =====================Alert===================== -->
-    <div class="container my-4">
-        <h2>Add a Note</h2>
-        <form action="/crudop/index.php" method="POST">
-            <div class="mb-3">
-                <label for="title" class="form-label">Note Title</label>
-                <input type="text" class="form-control" id="title" name="new_title" aria-describedby="emailHelp">
-                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
-            </div>
+            <!-- =====================Alert===================== -->
+            <div class="container my-4">
+                <h2>Add a Note</h2>
+                <form action="/crudop/index.php" method="POST">
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Note Title</label>
+                        <input type="text" class="form-control" id="title" name="new_title" aria-describedby="emailHelp">
+                        <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                    </div>
 
-            <div class="mb-3">
-                <label for="desc" class="form-label">Note Description</label>
-                <textarea class="form-control" id="description" name="new_description" rows="3"></textarea>
+                    <div class="mb-3">
+                        <label for="desc" class="form-label">Note Description</label>
+                        <textarea class="form-control" id="description" name="new_description" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add note</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Add note</button>
-        </form>
-    </div>
-    <div class="container">
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Sno</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+            <div class="container my-4">
+                <!-- --------------------TABLE---------------------------------- -->
+                <table class="table" id="myTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">Sno</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                 $sql = "SELECT * FROM `notes`";
                 $result = mysqli_query($conn, $sql);
+                $sno = 0;
+                // LOOP to put data in table
                 while ($row = mysqli_fetch_assoc($result)) {
+                    $sno = $sno + 1;
                     echo "<tr>
-                    <th scope='row'> " . $row['sno'] . " </th>
+                    <th scope='row'> " . $sno . " </th>
                     <td> " . $row['title'] . " </td>
                     <td> " . $row['description'] . " </td>
-                    <td> Actions </td>
+                    <td><button class='edit btn btn-sm btn-primary'>Edit</button> <a href='/del'>Delete</a></td>
                     </tr>";
                 }
+                
+                
                 ?>
 
 
-            </tbody>
-        </table>
-    </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+                    </tbody>
+                </table>
+                <!-- --------------------TABLE---------------------------------- -->
+            </div>
+            <hr>
+            <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+            <!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+            <!-- Option 2: Separate Popper and Bootstrap JS -->
+            <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
     -->
-</body>
 
-</html>
+    <script>
+            edits = document.getElementsByClassName('edit');
+            Array.from(edits).forEach((element) => {
+                element.addEventlistener("click", (e) => {
+                    console.log("edit", e);
+                })
+            })
+        </script> 
+    </body>
+
+    </html>
